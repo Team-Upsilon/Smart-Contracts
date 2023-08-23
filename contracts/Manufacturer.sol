9c34f25bc6201b168aa578ae86197700f08c0dde
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-08-24 01:34:30 +0530

    updated manufacturer.sol

--- contracts/Manufacturer.sol
+++ contracts/Manufacturer.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./BatchScheduler.sol";
 import "./Admin.sol";
-
+ 
 contract Manufacturer {
     BatchScheduler private batchSchedulerContract;
     Admin private adminContract;
