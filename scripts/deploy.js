671faa7e6ce73bbe5dc583801f8994d760e211b5
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-09-06 18:41:40 +0530

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,25 +6,25 @@ async function main() {
   const Admin = await ethers.deployContract("Admin");
   console.log("Admin: ", Admin.target);
 
-  const Inventory = await ethers.deployContract("Inventory", [Admin.target]);
+  const Inventory = await ethers.deployContract("Inventory");
   console.log("Inventory: ", Inventory.target);
 
-  const Supplier = await ethers.deployContract("Supplier", [Inventory.target, Admin.target]);
+  const Supplier = await ethers.deployContract("Supplier", [Inventory.target]);
   console.log("Supplier: ", Supplier.target);
 
   const BatchScheduler = await ethers.deployContract("BatchScheduler");
   console.log("BatchScheduler: ", BatchScheduler.target);
 
-  const Manufacturer = await ethers.deployContract("Manufacturer", [BatchScheduler.target, Admin.target]);
+  const Manufacturer = await ethers.deployContract("Manufacturer", [BatchScheduler.target]);
   console.log("Manufacturer: ", Manufacturer.target);
 
-  const Inspector = await ethers.deployContract("Inspector", [Supplier.target, Admin.target]);
+  const Inspector = await ethers.deployContract("Inspector", [Supplier.target]);
   console.log("Inspector: ", Inspector.target);
 
-  const Transporter = await ethers.deployContract("Transporter", [Supplier.target, Manufacturer.target, Admin.target]);
+  const Transporter = await ethers.deployContract("Transporter", [Supplier.target, Manufacturer.target]);
   console.log("Transporter: ", Transporter.target);
 
-  const RealTimeMonitoring = await ethers.deployContract("RealTimeMonitoring", [Manufacturer.target, Admin.target]);
+  const RealTimeMonitoring = await ethers.deployContract("RealTimeMonitoring", [Manufacturer.target]);
   console.log("RealTimeMonitoring: ", RealTimeMonitoring.target);
 
 }
